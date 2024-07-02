16bb6460881a674172aa1da717204fc85569e6d0
Benard <benmwash700@gmail.com>
2024-07-02 22:10:50 +0300

    phase 1 review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your code in this file!
+const currentUser = 'Renee Sinclair';
+
+const welcomeMessage = 'Welcome to Flatbook, ${Renee Sinclair}!';
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+
+
+
+
+
+
+
+
+
